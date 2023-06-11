653e6ea5dded1e91ad4fc00276d183206b3bdb6f
data04190 <data04190@naver.com>
2023-06-11 14:51:15 +0900

    Member 데이터 타입 정의

---
+++ myapp/app-11/src/main/java/bitcamp/myapp/vo/Member.java
@@ -0,0 +1,13 @@
+package bitcamp.myapp.vo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Member {
+  public int no;
+  public String studentName;
+  public LocalTime entryTime;
+  public LocalTime exitTime;
+  public LocalTime studyTime;
+  public String lateStatus;
+}
