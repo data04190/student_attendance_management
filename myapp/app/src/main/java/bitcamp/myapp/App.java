ad8120ce40ec2486e14f79815592e8d3597212c0
data04190 <data04190@naver.com>
2023-06-14 16:27:37 +0900

    게시글 CRUD 구현

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,6 @@
 package bitcamp.myapp;
 
+import bitcamp.myapp.handler.BoardHandler;
 import bitcamp.myapp.handler.MemberHandler;
 import bitcamp.util.Prompt;
 
@@ -13,12 +14,12 @@ public class App {
 
     while (true) {
       String menuNo = Prompt.inputString("main> ");
-      if(menuNo.equals("6")) {
+      if (menuNo.equals("99")) {
         break;
       } else if (menuNo.equals("menu")) {
         printMenu();
       } else if (menuNo.equals("1")) {
-        MemberHandler.inputMember();     
+        MemberHandler.inputMember();
       } else if (menuNo.equals("2")) {
         MemberHandler.printMembers();
       } else if (menuNo.equals("3")) {
@@ -27,6 +28,16 @@ public class App {
         MemberHandler.updateMember();
       } else if (menuNo.equals("5")) {
         MemberHandler.deleteMember();
+      } else if (menuNo.equals("6")) {
+        BoardHandler.inputBoard();
+      } else if (menuNo.equals("7")) {
+        BoardHandler.printBoards();
+      } else if (menuNo.equals("8")) {
+        BoardHandler.viewBoard();
+      } else if (menuNo.equals("9")) {
+        BoardHandler.updateBoard();
+      } else if (menuNo.equals("10")) {
+        BoardHandler.deleteBoard();
       } else {
         System.out.println(menuNo);
       }
@@ -41,7 +52,12 @@ public class App {
     System.out.println("3. 회원조회");
     System.out.println("4. 회원변경");
     System.out.println("5. 회원삭제");
-    System.out.println("6. 종료");
+    System.out.println("6. 게시글등록");
+    System.out.println("7. 게시글목록");
+    System.out.println("8. 게시글조회");
+    System.out.println("9. 게시글변경");
+    System.out.println("10. 게시글삭제");
+    System.out.println("99. 종료");
 
   }
 
@@ -50,12 +66,4 @@ public class App {
     System.out.println("=================================");
   }
 
-  static boolean promptContinue() {
-
-    String response = Prompt.inputString("계속 하시겠습니까?(Y/n) ");
-    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
-      return false;
-    }
-    return true;
-  }
 }
