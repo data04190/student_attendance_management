2afdf85b2603212ce5d8a9770b1ad0e374d68fc3
data04190 <data04190@naver.com>
2023-06-27 19:22:16 +0900

    Iterator 디자인 패턴 활용

---
+++ myapp/app-25/src/main/java/bitcamp/util/AbstractList.java
@@ -0,0 +1,72 @@
+package bitcamp.util;
+
+public abstract class AbstractList<E> implements List<E> {
+
+  protected int size;
+
+  @Override
+  public int size() {
+    return this.size;
+  }
+
+  protected boolean isValid(int index) {
+    return index >= 0 && index < this.size;
+  }
+
+  @Override
+  public Iterator<E> iterator() {
+
+
+    return new Iterator<E>() {
+      int cursor;
+
+      @Override
+      public boolean hasNext() {
+        return cursor < AbstractList.this.size();
+      }
+
+      @Override
+      public E next() {
+        return AbstractList.this.get(cursor++);
+      }
+    };
+  }
+
+  // static nested class
+  static class ListIterator2<T> implements Iterator<T> {
+    List<T> list;
+    int cursor;
+
+    public ListIterator2(List<T> list) {
+      this.list = list;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return cursor < list.size();
+    }
+
+    @Override
+    public T next() {
+      return list.get(cursor++);
+    }
+  }
+
+  // non-static nested class = inner class
+  class ListIterator3<T> implements Iterator<T> {
+
+    int cursor;
+
+
+    @Override
+    public boolean hasNext() {
+      return cursor < AbstractList.this.size();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public T next() {
+      return (T) AbstractList.this.get(cursor++);
+    }
+  }
+}
