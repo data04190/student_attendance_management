a32593d920f9f62badac481dcfa9c1c890328fa1
data04190 <data04190@naver.com>
2023-06-30 16:59:28 +0900

    Java Stream API로 교체

---
+++ myapp/app-31/src/main/java/bitcamp/myapp/handler/MemberListListener.java
@@ -0,0 +1,32 @@
+package bitcamp.myapp.handler;
+
+import java.util.Iterator;
+import java.util.List;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.BreadcrumbPrompt;
+
+
+public class MemberListListener extends AbstractMemberListener {
+
+  public MemberListListener(List<Member> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+
+    System.out.println("=====================================================================");
+    System.out.println("\t날짜\t이름\t입실 시간  퇴실 시간  스터디 시간  지각 여부");
+    System.out.println("=====================================================================");
+
+    // 목록에서 데이터를 대신 꺼내주는 객체를 얻는다.
+    Iterator<Member> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Member m = iterator.next();
+      System.out.printf("%-3d %-10s %-7s %-10s %-11s %-13s %-10s\n", m.getNo(), m.getDate(),
+          m.getStudentName(), m.getEntryTime(), m.getExitTime(), m.getStudyTime(),
+          m.getLateStatus());
+    }
+  }
+
+}
