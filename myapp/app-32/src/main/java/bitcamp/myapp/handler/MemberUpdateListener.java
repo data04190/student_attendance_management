2d27da552d9b44b535847aed4e4dd8564bd18c8e
data04190 <data04190@naver.com>
2023-07-01 01:09:50 +0900

    객체 직렬화 - ObjectInput/OutputStream 사용

---
+++ myapp/app-32/src/main/java/bitcamp/myapp/handler/MemberUpdateListener.java
@@ -0,0 +1,31 @@
+package bitcamp.myapp.handler;
+
+import java.time.LocalTime;
+import java.util.List;
+import bitcamp.myapp.vo.Member;
+import bitcamp.util.BreadcrumbPrompt;
+
+public class MemberUpdateListener extends AbstractMemberListener {
+
+  public MemberUpdateListener(List<Member> list) {
+    super(list);
+  }
+
+  @Override
+  public void service(BreadcrumbPrompt prompt) {
+    int memberNo = prompt.inputInt("번호? ");
+
+    Member m = this.findBy(memberNo);
+    if (m == null) {
+      System.out.println("해당 번호의 회원이 없습니다.");
+      return;
+    }
+
+    m.setStudentName(prompt.inputString("이름(%s)? ", m.getStudentName()));
+    m.setEntryTime(LocalTime.parse(prompt.inputString("입실 시간(%s)? ", m.getEntryTime())));
+    m.setExitTime(LocalTime.parse(prompt.inputString("퇴실 시간(%s)? ", m.getExitTime())));
+    m.setStudyTime(LocalTime.parse(prompt.inputString("스터디 시간(%s)? ", m.getStudyTime())));
+    m.setLateStatus();
+  }
+
+}
