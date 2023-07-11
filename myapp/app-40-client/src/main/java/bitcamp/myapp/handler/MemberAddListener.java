fc0e4bce8eb657533bb457d32419140e4b22ed7b
data04190 <data04190@naver.com>
2023-07-11 19:20:28 +0900

    DateTimeParseException 예외 처리

--- myapp/app-40-client/src/main/java/bitcamp/myapp/handler/MemberAddListener.java
+++ myapp/app-40-client/src/main/java/bitcamp/myapp/handler/MemberAddListener.java
@@ -2,6 +2,7 @@ package bitcamp.myapp.handler;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
 import bitcamp.util.BreadcrumbPrompt;
@@ -17,11 +18,37 @@ public class MemberAddListener implements MemberActionListener {
 
   public void service(BreadcrumbPrompt prompt) {
     Member m = new Member();
+
     m.setStudentName(prompt.inputString("이름? "));
     m.setDate(LocalDate.now());
-    m.setEntryTime(LocalTime.parse(prompt.inputString("입실 시간(HH:MM) ")));
-    m.setExitTime(LocalTime.parse(prompt.inputString("퇴실 시간(HH:MM) ")));
-    m.setStudyTime(LocalTime.parse(prompt.inputString("스터디 시간(HH:MM) ")));
+
+    while (true) {
+      try {
+        m.setEntryTime(LocalTime.parse(prompt.inputString("입실 시간(HH:MM) ")));
+        break;
+      } catch (DateTimeParseException e) {
+        System.out.println("잘못된 형식입니다. 다시 입력해주세요.");
+      }
+    }
+
+    while (true) {
+      try {
+        m.setExitTime(LocalTime.parse(prompt.inputString("퇴실 시간(HH:MM) ")));
+        break;
+      } catch (DateTimeParseException e) {
+        System.out.println("잘못된 형식입니다. 다시 입력해주세요.");
+      }
+    }
+
+    while (true) {
+      try {
+        m.setStudyTime(LocalTime.parse(prompt.inputString("스터디 시간(HH:MM) ")));
+        break;
+      } catch (DateTimeParseException e) {
+        System.out.println("잘못된 형식입니다. 다시 입력해주세요.");
+      }
+    }
+
     m.setLateStatus();
 
     memberDao.insert(m);
