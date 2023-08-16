451d1ed2dbc3aef700c3163dcfff7248d0eee5be
data04190 <data04190@naver.com>
2023-08-16 23:46:15 +0900

    웹 애플리케이션 자바 표준 기술 JavaEE 도입

---
+++ myapp/app-56-server/src/main/java/bitcamp/myapp/handler/InitServlet.java
@@ -0,0 +1,46 @@
+package bitcamp.myapp.handler;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import bitcamp.myapp.dao.AttendanceDao;
+import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.MemberDao;
+import bitcamp.myapp.dao.MySQLAttendanceDao;
+import bitcamp.myapp.dao.MySQLBoardDao;
+import bitcamp.myapp.dao.MySQLMemberDao;
+import bitcamp.util.SqlSessionFactoryProxy;
+
+@WebServlet(value = "/init", loadOnStartup = 1)
+
+public class InitServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  public static SqlSessionFactory sqlSessionFactory;
+  public static BoardDao boardDao;
+  public static MemberDao memberDao;
+  public static AttendanceDao attendanceDao;
+
+
+  @Override
+  public void init() throws ServletException {
+    System.out.println("InitServlet.init() 호출됨!");
+
+    try {
+      sqlSessionFactory = new SqlSessionFactoryProxy(new SqlSessionFactoryBuilder()
+          .build(Resources.getResourceAsStream("bitcamp/myapp/config/mybatis-config.xml")));
+
+      boardDao = new MySQLBoardDao(sqlSessionFactory);
+      memberDao = new MySQLMemberDao(sqlSessionFactory);
+      attendanceDao = new MySQLAttendanceDao(sqlSessionFactory);
+
+    } catch (Exception e) {
+      System.out.println("InitServlet.init() 실행 중 오류 발생!");
+      e.printStackTrace();
+    }
+  }
+}
