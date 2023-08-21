35346c5c6d053d4ffa7b3e78184ddf403f234f13
data04190 <data04190@naver.com>
2023-08-21 20:08:12 +0900

    NAVER CLOUD Image Optimizer 이용하여 썸네일 이미지 생성

---
+++ myapp/app-61-common/src/main/java/bitcamp/myapp/dao/BoardDao.java
@@ -0,0 +1,27 @@
+package bitcamp.myapp.dao;
+
+import java.util.List;
+import bitcamp.myapp.vo.AttachedFile;
+import bitcamp.myapp.vo.Board;
+
+public interface BoardDao {
+  void insert(Board board);
+
+  List<Board> findAll();
+
+  Board findBy(int no);
+
+  int update(Board board);
+
+  int updateCount(Board board);
+
+  int delete(Board board);
+
+  int insertFiles(Board board);
+
+  AttachedFile findFileBy(int no);
+
+  int deleteFile(int fileNo);
+
+
+}
---
+++ myapp/app-61-common/src/main/java/bitcamp/myapp/vo/Member.java
@@ -0,0 +1,83 @@
+package bitcamp.myapp.vo;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Member implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  private int no;
+  private String name;
+  private String email;
+  private String password;
+  private int level;
+  private String photo;
+
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(no);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Member other = (Member) obj;
+    return no == other.no;
+  }
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public String getPhoto() {
+    return photo;
+  }
+
+  public void setPhoto(String photo) {
+    this.photo = photo;
+  }
+
+}
