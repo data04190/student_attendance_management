590c5981b7e0a755543271312dcd003bc30f1fb6
data04190 <data04190@naver.com>
2023-09-03 23:10:00 +0900

    Spring IoC 컨테이너 이용하여 페이지 컨트롤러 관리

--- myapp/app-68-server/src/main/java/bitcamp/myapp/service/NcpObjectStorageService.java
+++ myapp/app-68-server/src/main/java/bitcamp/myapp/service/NcpObjectStorageService.java
@@ -1,8 +1,6 @@
-package bitcamp.util;
+package bitcamp.myapp.service;
 
-import java.io.InputStream;
-import java.util.UUID;
-import javax.servlet.http.Part;
+import bitcamp.myapp.config.NcpConfig;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
@@ -11,20 +9,24 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Part;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Component
 public class NcpObjectStorageService {
   final AmazonS3 s3;
 
   public NcpObjectStorageService(NcpConfig ncpConfig) {
-
-    System.out.println(ncpConfig.getAccessKey());
-
+    System.out.println("NcpObjectStorageService() 호출됨!");
     s3 = AmazonS3ClientBuilder.standard()
-        .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
-            ncpConfig.getEndPoint(), ncpConfig.getRegionName()))
-        .withCredentials(new AWSStaticCredentialsProvider(
-            new BasicAWSCredentials(ncpConfig.getAccessKey(), ncpConfig.getSecretKey())))
-        .build();
+            .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                    ncpConfig.getEndPoint(), ncpConfig.getRegionName()))
+            .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                    ncpConfig.getAccessKey(), ncpConfig.getSecretKey())))
+            .build();
   }
 
   public String uploadFile(String bucketName, String dirPath, Part part) {
@@ -38,18 +40,19 @@ public class NcpObjectStorageService {
       ObjectMetadata objectMetadata = new ObjectMetadata();
       objectMetadata.setContentType(part.getContentType());
 
-      PutObjectRequest objectRequest =
-          new PutObjectRequest(bucketName, dirPath + filename, fileIn, objectMetadata)
-              .withCannedAcl(CannedAccessControlList.PublicRead);
+      PutObjectRequest objectRequest = new PutObjectRequest(
+              bucketName,
+              dirPath + filename,
+              fileIn,
+              objectMetadata).withCannedAcl(CannedAccessControlList.PublicRead);
 
       s3.putObject(objectRequest);
 
-      // return s3.getUrl(bucketName, dirPath + filename).toString();
+      //return s3.getUrl(bucketName, dirPath + filename).toString();
       return filename;
 
     } catch (Exception e) {
       throw new RuntimeException("파일 업로드 오류", e);
     }
   }
-
 }
